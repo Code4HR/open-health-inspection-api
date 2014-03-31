5eaf6378bd4088b9523675a9376fce214c8e4d2a
ttavenner <tommy.tavenner@gmail.com>
2014-03-31 08:12:54 -0400

    reformat return coordinates

--- API.py
+++ API.py
@@ -1,14 +1,13 @@
 import mongolab
-from bson.objectid import ObjectId
 import json
+import re
+from bson.objectid import ObjectId
 from flask import Flask, Response, url_for, request, current_app
 from functools import wraps
-import re
 
 
 app = Flask(__name__)
-app.config['TESTING'] = True
-app.debug = True
+
 
 try:
     db = mongolab.connect()
@@ -17,7 +16,7 @@ except ValueError:
 
 
 def support_jsonp(f):
-    #Wraps JSONified output for JSONP
+    """Wraps JSONified output for JSONP"""
     @wraps(f)
     def decorated_function(*args, **kwargs):
         callback = request.args.get('callback', False)
@@ -26,7 +25,6 @@ def support_jsonp(f):
             return current_app.response_class(content, mimetype='application/json')
         else:
             return Response(f(*args, **kwargs), mimetype='application/json')
-
     return decorated_function
 
 
@@ -37,121 +35,128 @@ def api_root():
     for rule in app.url_map.iter_rules():
         if rule.endpoint != 'static':
             links.append(rule.rule)
-
     return json.dumps(links)
 
 
 @app.route('/vendors')
 @support_jsonp
 def api_vendors():
-    data = db.va.find({}, {'name': 1, 'address': 1, 'geo.coordinates': 1})
+    data = db.va.find({}, {'name': 1,
+                           'address': 1,
+                           'geo.coordinates': 1})
     if data.count() == 0:
         resp = json.dumps({'status': '204', 'message': 'no results returned'})
     else:
         vendor_list = {}
         for item in data:
-            url = url_for("api_vendor", vendorid=str(item["_id"]))
-            vendor_list[str(item["_id"])] = {'url': url,
-                                             'name': item["name"],
-                                             'address': item["address"],
-                                             'coordinates': item["geo"]["coordinates"]}
-
+            url = url_for('api_vendor', vendorid=str(item['_id']))
+            vendor_list[str(item['_id'])] = {'url': url,
+                                             'name': item['name'],
+                                             'address': item['address'],
+                                             'coordinates': {
+                                                 'latitude': item['geo']['coordinates'][0],
+                                                 'longitude': item['geo']['coordinates'][1]}}
         resp = json.dumps(vendor_list)
-
     return resp
 
 
 @app.route('/vendors/textsearch/<searchstring>')
 @support_jsonp
 def api_vendor_text_search(searchstring):
-
     regex = re.compile(re.escape(searchstring), re.IGNORECASE)
-    data = db.va.find({ '$or': [{ 'name': regex}, {'address': regex}, {'city': regex}]},
-                      {'name': 1, 'address': 1, 'geo.coordinates': 1})
-
+    data = db.va.find({'$or':
+                            [{'name': regex},
+                             {'address': regex},
+                             {'city': regex}]},
+                      {'name': 1,
+                       'address': 1,
+                       'geo.coordinates': 1})
     if data.count() == 0:
         resp = json.dumps({'status': '204', 'message': 'no results returned'})
     else:
         vendor_list = {}
         for item in data:
-            url = url_for('api_vendor', vendorid=item["_id"])
-            vendor_list[str(item["_id"])] = {'url': url,
-                                             'name': item["name"],
-                                             'address': item["address"],
-                                             'coordinates': item["geo"]["coordinates"]}
-
+            url = url_for('api_vendor', vendorid=item['_id'])
+            vendor_list[str(item['_id'])] = {'url': url,
+                                             'name': item['name'],
+                                             'address': item['address'],
+                                             'coordinates': {
+                                                 'latitude': item['geo']['coordinates'][0],
+                                                 'longitude': item['geo']['coordinates'][1]}}
         resp = json.dumps(vendor_list)
-
     return resp
 
 
 @app.route('/vendors/geosearch/<lng>/<lat>/<dist>')
 @support_jsonp
 def api_vendor_geo_search(lng, lat, dist):
-    data = db.va.find({'geo': {'$within': { '$center': [[ float(lng), float(lat)], int(dist)]}}},
-                      {'name': 1, 'address': 1, 'geo.coordinates': 1})
+    data = db.va.find({'geo':
+                           {'$within':
+                                   {'$center': [[ float(lng), float(lat)], int(dist)]}}},
+                      {'name': 1,
+                       'address': 1,
+                       'geo.coordinates': 1})
     if data.count() == 0:
         resp = json.dumps({'status': '204', 'message': 'no results returned'})
     else:
         vendors = {}
         for item in data:
-            url = url_for("api_vendor", vendorid=str(item["_id"]))
-            vendors[str(item["_id"])] = {'url': url,
-                                         'name': item["name"],
-                                         'address': item["address"],
-                                         'coordinates': item["geo"]["coordinates"]}
+            url = url_for('api_vendor', vendorid=str(item['_id']))
+            vendors[str(item['_id'])] = {'url': url,
+                                         'name': item['name'],
+                                         'address': item['address'],
+                                         'coordinates': {
+                                                 'latitude': item['geo']['coordinates'][0],
+                                                 'longitude': item['geo']['coordinates'][1]}}
         resp = json.dumps(vendors)
-
     return resp
 
 
 @app.route('/vendor/<vendorid>')
 @support_jsonp
 def api_vendor(vendorid):
-
     data = db.va.find({'_id': ObjectId(vendorid)}, {'name': 1,
                                                     'address': 1,
                                                     'inspections.0': {'$slice': 1},
                                                     'geo.coordinates': 1}).sort('inspection.date')
-
     if data.count() == 1:
         item = data[0]
-        inspection = item["inspections"][0]
-        vendor = {str(item["_id"]): {'name': item["name"],
-                                     'address': item["address"],
-                                     'last_inspection_date': inspection["date"],
+        inspection = item['inspections'][0]
+        vendor = {str(item['_id']): {'name': item['name'],
+                                     'address': item['address'],
+                                     'last_inspection_date': inspection['date'],
                                      'violations': inspection["violations"],
-                                     'coordinates': item["geo"]["coordinates"]}}
+                                     'coordinates': {
+                                                 'latitude': item['geo']['coordinates'][0],
+                                                 'longitude': item['geo']['coordinates'][1]}}}
         resp = json.dumps(vendor)
     elif data.count() > 1:
         resp = json.dumps({'status': '300'})
     else:
         resp = json.dumps({'status': '204'})
-
     return resp
 
 
 @app.route('/inspections/<vendorid>')
 @support_jsonp
 def api_inspections(vendorid):
-
     data = db.va.find({'_id': ObjectId(vendorid)}, {'name': 1, 'address': 1,
                                                     'last_inspection_date': 1,
                                                     'inspections': 1,
                                                     'geo.coordinates': 1})
-
     if data.count() == 1:
-        vendor = {str(data[0]["_id"]): {'name': data[0]["name"],
-                                        'address': data[0]["address"],
-                                        'last_inspection_date': data[0]["last_inspection_date"],
-                                        'inspections': data[0]["inspections"],
-                                        'coordinates': data[0]["geo"]["coordinates"]}}
+        vendor = {str(data[0]["_id"]): {'name': data[0]['name'],
+                                        'address': data[0]['address'],
+                                        'last_inspection_date': data[0]['last_inspection_date'],
+                                        'inspections': data[0]['inspections'],
+                                        'coordinates': {
+                                                 'latitude': data[0]['geo']['coordinates'][0],
+                                                 'longitude': data[0]['geo']['coordinates'][1]}}}
         resp = json.dumps(vendor)
     elif data.count() > 1:
         resp = json.dumps({'status': '300'})
     else:
         resp = json.dumps({'status': '204'})
-
     return resp
 
 
