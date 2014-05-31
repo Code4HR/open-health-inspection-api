c4b1482075b713e682c39a4f798fba33494e0a58
ttavenner <tommy.tavenner@gmail.com>
2014-05-31 11:23:32 -0400

    first development commit

--- API.py
+++ API.py
@@ -1,7 +1,6 @@
 import mongolab
 import json
 import re
-
 #import csv
 #import zipfile
 from math import radians, cos, sin, atan2, sqrt
@@ -67,7 +66,6 @@ def api_vendors():
 
     if request.args.get('limit') is not None:
         limit = int(request.args.get('limit'))
-
     if request.args.get('name') is not None:
         query.update({'name': re.compile(re.escape(request.args.get('name')), re.IGNORECASE)})
     if request.args.get('address') is not None:
@@ -106,7 +104,6 @@ def api_vendors():
                                                                            float(request.args.get('lat')),
                                                                            item['geo']['coordinates'][0],
                                                                            item['geo']['coordinates'][1]), 2)
-        print vendor_list
 
         if request.args.get('pretty') == 'true':
             resp = json.dumps(vendor_list, indent=4)
@@ -126,14 +123,14 @@ def api_vendor(vendorid):
     if data.count() == 1:
         item = data[0]
         inspection = item['inspections'][0]
-        vendor = {str(item['_id']): {'name': item['name'],
+        vendor = OrderedDict({str(item['_id']): {'name': item['name'],
                                      'address': item['address'],
                                      'type': item['type'],
                                      'last_inspection_date': inspection['date'].strftime('%d-%b-%Y'),
                                      'violations': inspection['violations'],
                                      'coordinates': {
                                                  'latitude': item['geo']['coordinates'][0],
-                                                 'longitude': item['geo']['coordinates'][1]}}}
+                                                 'longitude': item['geo']['coordinates'][1]}}})
         resp = json.dumps(vendor)
     elif data.count() > 1:
         resp = json.dumps({'status': '300'})
@@ -142,31 +139,51 @@ def api_vendor(vendorid):
     return resp
 
 
-@app.route('/inspections/<vendorid>')
+@app.route('/inspections')
 @support_jsonp
-def api_inspections(vendorid):
-    data = db.va.find({'_id': ObjectId(vendorid)}, {'name': 1,
-                                                    'address': 1,
-                                                    'type': 1,
-                                                    'last_inspection_date': 1,
-                                                    'inspections': 1,
-                                                    'geo.coordinates': 1})
-    if data.count() == 1:
-        vendor = {str(data[0]["_id"]): {'name': data[0]['name'],
-                                        'address': data[0]['address'],
-                                        'type': data[0]['type'],
-                                        'last_inspection_date': data[0]['last_inspection_date'].strftime('%d-%b-%Y'),
-                                        'inspections': data[0]['inspections'],
-                                        'coordinates': {
-                                                 'latitude': data[0]['geo']['coordinates'][0],
-                                                 'longitude': data[0]['geo']['coordinates'][1]}}}
-        resp = json.dumps(vendor)
-    elif data.count() > 1:
-        resp = json.dumps({'status': '300'})
+def api_inspections():
+
+    limit = 500
+    query = {}
+
+    if request.args.get('vendorid') is not None:
+        query.update({'_id': ObjectId(request.args.get('vendorid'))})
+    if request.args.get('before') is not None:
+        query.update({'inspections.date': {'$lte': datetime.strptime(request.args.get('before'), '%d-%m-%Y')}})
+    if request.args.get('after') is not None:
+        query.update({'inspections.date': {'$gte': datetime.strptime(request.args.get('after'), '%d-%m-%Y')}})
+    print query
+    data = db.va.find(query, {'name': 1,
+                              'address': 1,
+                              'type': 1,
+                              'last_inspection_date': 1,
+                              'inspections.$': 1,
+                              'geo.coordinates': 1}).limit(limit)
+    if data.count() > 0:
+        vendor_list = OrderedDict()
+        for item in data:
+            inspections = OrderedDict()
+            for inspection in item['inspections']:
+                inspections.update(OrderedDict({'date': inspection['date'].strftime('%d-%b-%Y'),
+                                                'violations': inspection['violations']}))
+
+            vendor_list[str(item["_id"])] = OrderedDict({'name': item['name'],
+                                                         'address': item['address'],
+                                                         'type': item['type']})
+            if item['last_inspection_date'] is not None:
+                vendor_list[str(item['_id'])].update({'last_inspection_date': item['last_inspection_date'].strftime('%d-%b-%Y')})
+            if inspections is not None:
+                vendor_list[str(item['_id'])].update({'inspections': inspections})
+            if 'geo' in item:
+                vendor_list[str(item['_id'])].update({'coordinates': { 'latitude': item['geo']['coordinates'][0],
+                                                                       'longitude': item['geo']['coordinates'][1]}})
+        if request.args.get('pretty') == 'true':
+            resp = json.dumps(vendor_list, indent=4)
+        else:
+            resp = json.dumps(vendor_list)
     else:
         resp = json.dumps({'status': '204'})
     return resp
-
 
 
 @app.route('/lives')
